3c99883bee2d5e42ff60c0e3b3093cdf2bd2d73d
Paulo <pauloantobe@live.com.ar>
2023-11-19 18:25:14 -0300

    agrego variables de entorno

--- src/servidor.js
+++ src/servidor.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hola mundo!');
 });
 
-app.get('/usuarios', (req, res) => {
-    res.send('Lista de usuarios');
-})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+
 
